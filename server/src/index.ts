59880e8d0ce2e43826dbe088630faf10dc47487b
ashbie <ashbie.b.mweemba@gmail.com>
2022-09-01 00:47:54 +0100

    connect the server with mongoDB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,27 @@
-import express from "express";
+import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
+import { connectDatabase } from "./database/index";
 import { typeDefs, resolvers } from "./graphql";
 
 
-const app = express();
 const port = 3000;
 
-const server = new ApolloServer({ typeDefs, resolvers });
-server.start().then(() => {
-    server.applyMiddleware({app, path: '/api'});
 
-    app.listen(port);
 
-    console.log(`[app]: http://localhost:${port}/api`);
-});
+const mount = async (app: Application) => {
+    const db = await connectDatabase();
+    const server = new ApolloServer({ typeDefs, resolvers, context: () => ({ db }) });
+    server.start().then(async () => {
+
+        server.applyMiddleware({app, path: '/api'});
+
+        app.listen(port);
+
+        console.log(`[app]: http://localhost:${port}/api`);
+
+        const listings = await db.listings.find({}).toArray();
+        console.log(listings);
+    });
+}
+
+mount(express());
